91c8ab5efafcef10bc4096d44bea86011ebe50ad
Henry Barker <barker.henry@maranathastudents.org>
2021-09-02 19:06:02 -0700

    good

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private bool canMove;
+    private Rigidbody2D theRB2D;
+
+    public bool grounded;
+    public LayerMask whatIsGrd;
+    public Transform grdChecker;
+    public float grdCheckerRad;
+
+    public float airTime;
+    public float airTimeCounter;
+
+    private Animator theAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theRB2D = GetComponent<Rigidbody2D>();
+        theAnimator = GetComponent<Animator>();
+
+        airTimeCounter = airTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            canMove = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(grdChecker.position, grdCheckerRad, whatIsGrd);
+
+        MovePlayer();
+        Jump();
+    }
+
+    void MovePlayer()
+    {
+        if (canMove)
+        {
+            theRB2D.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, theRB2D.velocity.y);
+
+            theAnimator.SetFloat("Speed", Mathf.Abs(theRB2D.velocity.x));
+
+            if(theRB2D.velocity.x > 0)
+            {
+                transform.localScale = new Vector2(1f, 1f);
+            }
+            else if(theRB2D.velocity.x < 0)
+            {
+                transform.localScale = new Vector2(-1f, 1f);
+            }
+        }
+    }
+
+    void Jump()
+    {
+        if (grounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+            {
+                theRB2D.velocity = new Vector2(theRB2D.velocity.x, jumpForce);
+            }
+        }
+
+        if(Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0))
+        {
+            if(airTimeCounter > 0)
+            {
+                theRB2D.velocity = new Vector2(theRB2D.velocity.x, jumpForce);
+                airTimeCounter -= Time.deltaTime;
+            }
+        }
+
+        if(Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonUp(0))
+        {
+            airTimeCounter = 0;
+        }
+
+        if (grounded)
+        {
+            airTimeCounter = airTime;
+        }
+
+        theAnimator.SetBool("Grounded", grounded);
+    }
+}
